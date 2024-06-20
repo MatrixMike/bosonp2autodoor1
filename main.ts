2cdaf7c5619f31fb96812b3b4d205211dff0d91a
Mike Hewitt <MatrixMike@users.noreply.github.com>
2024-06-20 22:29:31 +1000

    fix my copy error -

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
         pins.servoWritePin(AnalogPin.P0, 0)
         basic.showIcon(IconNames.Heart)
         basic.pause(2000)
-        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.Yes)
     } else {
         pins.servoWritePin(AnalogPin.P0, 120)
     }
